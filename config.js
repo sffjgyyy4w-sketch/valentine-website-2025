27731cdbf8fdc74789b43ba89a80a1bc68e4c56b
sffjgyyy4w-sketch <sffjgyyy4w@privaterelay.appleid.com>
2026-05-18 23:43:54 +0300

    يحوش

--- config.js
+++ config.js
@@ -5,17 +5,17 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "يحوش"
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "تبين تاكلين معاي فلافل؟",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        hearts: ['🧆', '✌️', '😴', '🔥', ''],  // Heart emojis
+        bears: ['', '']                       // Cute bear emojis
     },
 
     // Questions and answers
